4a3952efa5506f1ba0c6573ee0e5d145f78e3813
EdsonAvelar <49203467+EdsonAvelar@users.noreply.github.com>
2019-05-21 21:02:26 -0300

    Update auc.py

--- auc.py
+++ auc.py
@@ -4,3 +4,27 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
 import matplotlib.pyplot as plt
+
+# Carregando Breast Cancer Dataset
+breast_cancer = load_breast_cancer()
+X = breast_cancer.data
+y = breast_cancer.target
+
+# Separando o Dataset
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.33, random_state=44)
+
+# Criando um modelo 
+clf = LogisticRegression(penalty='l2', C=0.1)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+# Accuracy
+print("Accuracy", metrics.accuracy_score(y_test, y_pred))
+
+#AUC Curve
+y_pred_probability = clf.predict_proba(X_test)[::,1]
+fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_probability)
+auc = metrics.roc_auc_score(y_test, y_pred_probability)
+plt.plot(fpr,tpr,label="data 1, auc="+str(auc))
+plt.legend(loc=4)
+plt.show()
