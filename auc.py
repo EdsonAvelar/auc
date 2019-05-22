97e36c869bf6139c24784fde533688b723c26249
EdsonAvelar <49203467+EdsonAvelar@users.noreply.github.com>
2019-05-21 21:00:06 -0300

    Create auc.py

---
+++ auc.py
@@ -0,0 +1,6 @@
+from sklearn import svm, datasets
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+import matplotlib.pyplot as plt
